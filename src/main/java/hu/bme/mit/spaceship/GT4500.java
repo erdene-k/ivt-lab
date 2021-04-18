8fb9a209f2fae234655f43106b59534d18739520
Erdenechuluun Khuderchuluun <tesoro.ec@gmail.com>
2021-04-18 16:28:32 +0200

    resolved conflict

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -88,14 +88,7 @@ public class GT4500 implements SpaceShip {
               
     	  }
     	  
-<<<<<<< HEAD
-    	  
-<<<<<<< HEAD
-=======
->>>>>>> branch-A
-=======
-    	  
->>>>>>> branch-B
+
         break;
     }
 
